06d9bd964bb5f0b67405d30b92f6699e529c8eea
Skeby <103804946+Skeby@users.noreply.github.com>
2024-01-14 19:29:35 +0100

    feat: setup headers for CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectToDB from "./db.js";
 import conversionController from "./controllers/conversionController.js";
 
@@ -9,6 +10,18 @@ const { urlencoded, json } = bodyParser;
 const port = process.env.PORT || 3000;
 const server = express();
 connectToDB("currency-converter");
+
+server.use(cors());
+
+// Add headers for CORS
+server.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // parse application/x-www-form-urlencoded
 server.use(urlencoded({ extended: false }));
